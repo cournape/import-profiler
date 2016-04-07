3eeff9df30f8c3f87d64764098316530f10c435e
David Cournapeau <cournape@gmail.com>
2016-04-07 16:20:40 +0100

    MAINT: add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+with open("README.md", "rt") as fp:
+    DESCRIPTION = fp.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="import-profiler",
+        version="0.0.1.dev0",
+        author="David Cournapeau",
+        author_email="cournape@gmail.com",
+        license="MIT",
+        install_requires=["tabulate >= 0.7.5"],
+        description="Import profiler to find bottlenecks in import times.",
+        long_description=DESCRIPTION,
+    )
