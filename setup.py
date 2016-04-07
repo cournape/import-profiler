c8a54d203649efa2f4f3145b8527d0c9e1f97246
David Cournapeau <cournape@gmail.com>
2016-04-07 16:29:41 +0100

    MAINT: start v0.0.3 branch.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "rt") as fp:
 if __name__ == "__main__":
     setup(
         name="import_profiler",
-        version="0.0.2",
+        version="0.0.3.dev0",
         author="David Cournapeau",
         author_email="cournape@gmail.com",
         license="MIT",
